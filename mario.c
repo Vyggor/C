68f34e02a6d08e32388a9c60ce5654e919177333
Vyggor_Tz <89152066+Vyggor@users.noreply.github.com>
2022-10-02 10:49:28 +0300

    Add files via upload
    Building blocks with hashes -mario like.

---
+++ mario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    char c = '#';
+
+    do
+    {
+        n = get_int("what's the height of the pyramid? ");
+    }
+    while (n < 1 || n > 8);
+
+    for (int i = 0; i < n; i++)
+    {
+        for ( int d = n - i - 1; d > 0; d--)
+        {
+            printf(" ");
+        }
+        for ( int j = 0; j < i + 1; j++){
+            printf("%c" , c);
+        }
+
+        printf("\n");
+    }
+}
+
+
+
